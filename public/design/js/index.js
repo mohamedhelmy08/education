0f01638fa14f64cbdfe409fe47ff7f80f41ce5c4
mohamedhelmy08 <mohamedhelmy2016@gmail.com>
2019-08-02 22:57:27 +0200

    First Commit only Design

---
+++ public/design/js/index.js
@@ -0,0 +1,7 @@
+// ======================== start tooltip =========================== -->
+$('.tt_reg').tooltip();
+
+$('.tt_large').tooltip({
+    template: '<div class="tooltip" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner large"></div></div>'
+});
+// ============================ end tooltip =========================== -->
